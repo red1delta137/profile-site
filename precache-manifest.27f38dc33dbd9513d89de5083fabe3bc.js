49ee228359164e0054a3dcdcfd7e90d809fef4b8
David Fletcher <red1delta137@gmail.com>
2023-02-04 18:10:06 -0500

    Updates

---
+++ precache-manifest.27f38dc33dbd9513d89de5083fabe3bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "befdd9fab75812a798eb",
+    "url": "/profile-site/static/css/main.89f9d208.chunk.css"
+  },
+  {
+    "revision": "befdd9fab75812a798eb",
+    "url": "/profile-site/static/js/main.befdd9fa.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/profile-site/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/profile-site/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "3381546559f16db8bcef",
+    "url": "/profile-site/static/js/runtime~main.33815465.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/profile-site/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b72b60f24fb890a6d85ee5cb4c6fcb13",
+    "url": "/profile-site/index.html"
+  }
+];
