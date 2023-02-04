4d3ed7bc0eac05816c4cace2424981842d0bc9a0
David Fletcher <red1delta137@gmail.com>
2023-02-04 18:12:54 -0500

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hi I'm David and I code.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
